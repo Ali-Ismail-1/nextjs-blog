aa172fbc6b6623b35e41e605d081ce5d5d88cfb5
Ali Ismail <aliismaildeveloper@gmail.com>
2022-02-11 12:08:23 -0800

    Updated Footer to GitHub link

--- components/footer.js
+++ components/footer.js
@@ -17,7 +17,8 @@ export default function Footer() {
               Read Documentation
             </a>
             <a
-              href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
+              href={`https://github.com/Ali-Ismail-1/blog-starter`}
+              //href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
               className="mx-3 font-bold hover:underline"
             >
               View on GitHub

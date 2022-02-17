5d793d283f27ed2b837813d4a88a98926e77b9bc
Ali Ismail <aliismaildeveloper@gmail.com>
2022-02-16 16:26:47 -0800

    Test PrismJS

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/index.css'
 import '../styles/blog.css'
+import '../styles/prism.css'
+import '../prism.js'
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />

89dbc6a8e17db60f60d9083f3648ac6c8bbd9a6b
Ali Ismail <aliismaildeveloper@gmail.com>
2022-02-16 16:53:02 -0800

    Layout and Container

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,15 @@
+import Layout from '../../components/layout'
+import Container from '../../components/container'
+
 export default function About() {
     return (
-        <div>
-            <h1>About this Blog</h1>
-            <p>This is the best blog ever</p>
-        </div>
+        <Layout>
+            <Container>
+            <div>
+                <h1>About this Blog</h1>
+                <p>This is the best blog ever</p>
+            </div>
+            </Container>
+        </Layout>
     );
 }

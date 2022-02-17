1b5bdb3a21c530b31bc6166ae201dac8b5b6aceb
Ali Ismail <aliismaildeveloper@gmail.com>
2022-02-16 20:04:15 -0800

    Comment About page

--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,15 @@
-import Layout from '../../components/layout'
-import Container from '../../components/container'
+// import Layout from '../../components/layout'
+// import Container from '../../components/container'
 
 export default function About() {
     return (
-        <Layout>
-            <Container>
+        // <Layout>
+        //     <Container>
             <div>
                 <h1>About this Blog</h1>
                 <p>This is the best blog ever</p>
             </div>
-            </Container>
-        </Layout>
+        //     </Container>
+        // </Layout>
     );
 }
